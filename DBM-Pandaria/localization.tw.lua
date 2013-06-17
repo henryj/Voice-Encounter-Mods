9fb6027b174f91bab383e7420d408f60141019b1
Henry J <m@jjw.in>
2013-06-17 17:27:20 +1000

    all the other non raid modules

---
+++ DBM-Pandaria/localization.tw.lua
@@ -0,0 +1,54 @@
+﻿if GetLocale() ~= "zhTW" then return end
+local L
+
+-----------------------
+-- Sha of Anger --
+-----------------------
+L= DBM:GetModLocalization(691)
+
+L:SetOptionLocalization({
+	SoundWOP			= "語音警告：重要技能",
+	RangeFrame			= "距離監視：$spell:119622",
+	HudMAP				= "高級定位監視(HUD)：$spell:119622",
+	SetIconOnMC			= DBM_CORE_AUTO_ICONS_OPTION_TEXT:format(119622)
+})
+
+L:SetMiscLocalization({
+	Pull				= "沒錯...沒錯!釋放你的怒火!試著擊敗我!"
+})
+
+-----------------------
+-- Salyis --
+-----------------------
+L= DBM:GetModLocalization(725)
+
+L:SetOptionLocalization({
+	SoundWOP			= "語音警告：重要技能"
+})
+
+L:SetMiscLocalization({
+	Pull				= "把他們的屍體帶給我!"
+})
+
+--------------
+-- Oondasta --
+--------------
+L= DBM:GetModLocalization(826)
+
+L:SetOptionLocalization({
+	SoundWOP			= "語音警告：重要技能",
+	RangeFrame			= "距離監視：$spell:137511"
+})
+
+L:SetMiscLocalization({
+	Pull				= "你們竟敢打擾我們的準備工作!贊達拉這次不會再被阻止了!"
+})
+
+---------------------------
+-- Nalak, The Storm Lord --
+---------------------------
+L= DBM:GetModLocalization(814)
+
+L:SetOptionLocalization({
+	RangeFrame			= "距離監視(10碼)：$spell:136340"
+})
